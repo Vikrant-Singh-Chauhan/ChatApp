934aca3a967e423bcdb34f882711b7eaca2fe32c
Vikrant Singh Chauhan <vikrantsingh11x@gmail.com>
2025-04-12 18:14:24 +0530

    add UserSeach page

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/pages/chat_page/chat_page.dart';
 import 'package:flutter/material.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -12,50 +14,162 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Colors.redAccent[100],
       body: Container(
-        margin: EdgeInsets.only(top: 30,left: 10),
+        margin: EdgeInsets.only(
+          top: 30,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Image.asset("images/wawe.png",height: 50,width: 50,),
-              Text(
-                "hello, ",
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "images/wawe.png",
+                    height: 50,
+                    width: 50,
+                  ),
+                  Text("hello, ",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center),
+                  Text(
+                    "Vikrant",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Container(
+                    margin: EdgeInsets.only(right: 20),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Icon(Icons.person, color: Colors.redAccent[100]),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Welcome to",
+                style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Text(
+                "Lets Chat",
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center
-              ),
-              Text(
-                "Vikrant",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 35,
                     fontWeight: FontWeight.bold),
               ),
-              Spacer(),
-              Container(
-                margin: EdgeInsets.only(right: 20),
-                padding: EdgeInsets.all(5),
-                decoration: BoxDecoration(color: Colors.white,
-                borderRadius: BorderRadius.circular(10)),
-                child: Icon(Icons.person,color: Colors.redAccent[100]),
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Welcome to",
-            style: TextStyle(
-                color: Colors.white60,
-                fontSize: 25,
-                fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),) ,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Expanded(
+                                                        /// search user here///
+              child: Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15)),
+                      color: Colors.white),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.grey[300],
+                            border: Border.all()),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon: Icon(Icons.search),
+                                hintText: "Search Username"),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20,),
+                                                               /// Searched user Container///
+                      GestureDetector(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(),));
+                        },
+                        child: Material(
+                          elevation: 4,
+                          borderRadius: BorderRadius.circular(10),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.red[100],
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                foregroundImage: AssetImage("images/photo.jpeg"),
+                                radius: 30,
+                                backgroundColor: Colors.green,
+                              ),
+                              title: Text(
+                                "Vikrant",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              subtitle:  Text(
+                                "where are you going today",
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              trailing: Column(
+                                children: [
+                                  Text(
+                                    "01:30",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Icon(Icons.keyboard_double_arrow_down_outlined,)
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
